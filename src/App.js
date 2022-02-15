b4f34cb31ff08a941d02292e72b485df0a682dce
Kevin Efraim <kevinefraim02@gmail.com>
2022-02-15 14:36:00 -0300

    funcion ppj

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import FormPj from "./components/formPj/FormPj";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Game from "./components/formPj/game/Game";
+import Game from "./components/game/Game";
 
 function App() {
   return (
     <Router>
-      <div className="text-center p-1 text-light">
-        <h1 className="mt-5">Ta Te Ti en React.js</h1>
-        <h2>Elige tu símbolo</h2>
-      </div>
+      <h1 className="text-center p-4 text-light">Ta Te Ti en React.js</h1>
       <Routes>
         <Route path="/" element={<FormPj />}></Route>
         <Route path="/game" element={<Game />}></Route>
--- src/components/formPj/FormPj.js
+++ src/components/formPj/FormPj.js
@@ -1,30 +1,77 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./styles.css";
+import "./FormPj.css";
 
 const FormPj = () => {
+  const [name1, setname1] = useState("");
+  const [name2, setname2] = useState("");
+  const [symbol1, setSymbol1] = useState("");
+  const [symbol2, setSymbol2] = useState("");
+  const [player1, setPlayer1] = useState({});
+  const [player2, setPlayer2] = useState({});
+
+  // } else if (symbol1 === "O") {
+  //   setSymbol2("X");
+  // }
+
+  const submitPj = (e) => {
+    e.preventDefault();
+    if (symbol1 === "X") {
+      setSymbol2("O");
+    } else if (symbol1 === "O") {
+      setSymbol2("X");
+    }
+  };
+
   return (
     <main>
       <div className="form-container">
-        <h3 className="text-center pt-2">Selecciona tu símbolo</h3>
-        <form className="form-players">
+        <h3 className="text-center  pt-2">Selecciona tu símbolo</h3>
+        <form className="form-players" onSubmit={submitPj}>
           <div className="form-item">
             <label htmlFor="player1">Tu:</label>
-            <input type="text" placeholder="Escribe tu nombre" />
+            <input
+              type="text"
+              placeholder="Escribe tu nombre"
+              onChange={({ target }) => setname1(target.value)}
+              value={name1}
+            />
           </div>
           <div className="form-item">
             <label htmlFor="player2">Rival:</label>
-            <input type="text" placeholder="Escribe tu nombre" />
+            <input
+              type="text"
+              onChange={({ target }) => setname2(target.value)}
+              value={name2}
+              placeholder="Escribe tu nombre"
+            />
+          </div>
+          <div className="btn-flex">
+            <div className="btn-container">
+              <button
+                type="button"
+                onClick={() => setSymbol1("X")}
+                className="btn-symbol text-primary"
+              >
+                X
+              </button>
+              <button
+                type="button"
+                onClick={() => setSymbol1("O")}
+                className="btn-symbol text-success"
+              >
+                O
+              </button>
+            </div>
+            <div className="btn-container">
+              {/* <Link to="game"> */}
+              <button type="submit" className="btn-play text-primary">
+                Jugar
+              </button>
+              {/* </Link> */}
+            </div>
           </div>
         </form>
-        <div className="btn-container">
-          <button className="btn-symbol text-primary">X</button>
-          <button className="btn-symbol text-success">O</button>
-        </div>
-        <div className="btn-container">
-          <Link to="game">
-            <button className="btn-play text-primary">Jugar</button>
-          </Link>
-        </div>
       </div>
     </main>
   );
