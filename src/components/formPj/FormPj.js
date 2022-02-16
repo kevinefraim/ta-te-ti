62563066e3b89ecdc9619ad2db9728e7d0f3ea1d
Kevin Efraim <kevinefraim02@gmail.com>
2022-02-16 13:57:19 -0300

    logica a la mitad

--- src/components/formPj/FormPj.js
+++ src/components/formPj/FormPj.js
@@ -1,35 +1,75 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Game from "../game/Game";
 import "./FormPj.css";
 
 const FormPj = () => {
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [activePj, setActivePj] = useState(false);
+  const [players, setPlayers] = useState(
+    JSON.parse(localStorage.getItem("players")) ?? []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("players", JSON.stringify(players));
+  }, [players]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setPlayers([name1, name2]);
+    setActivePj(true);
+    setName1("");
+    setName2("");
+  };
+  console.log(players);
   return (
     <main>
       <div className="form-container">
-        <h3 className="text-center  pt-2">Selecciona tu símbolo</h3>
-        <form className="form-players">
-          <div className="form-item">
-            <label htmlFor="player1">Tu:</label>
-            <input type="text" placeholder="Escribe tu nombre" />
+        <h3 className="text-center  pt-2">Inserte jugadores</h3>
+        <form onSubmit={submitForm} className="form-players">
+          <div className="btn-container">
+            <p type="button" className="btn-symbol text-center text-primary">
+              X
+            </p>
+            <p type="button" className="btn-symbol text-center text-success">
+              O
+            </p>
           </div>
-          <div className="form-item">
-            <label htmlFor="player2">Rival:</label>
-            <input type="text" placeholder="Escribe tu nombre" />
-          </div>
-          <div className="btn-flex">
-            <div className="btn-container">
-              <button type="button" className="btn-symbol text-primary">
-                X
-              </button>
-              <button type="button" className="btn-symbol text-success">
-                O
-              </button>
+          <div className="name-inputs">
+            <div className="form-item">
+              <label htmlFor="player1">Tu:</label>
+              <input
+                type="text"
+                placeholder="Escribe tu nombre"
+                onChange={(e) => setName1(e.target.value)}
+                value={name1}
+              />
             </div>
-            <div className="btn-container">
+            <div className="form-item">
+              <label htmlFor="player2">Rival:</label>
+              <input
+                type="text"
+                placeholder="Escribe tu nombre"
+                onChange={(e) => setName2(e.target.value)}
+                value={name2}
+              />
+            </div>
+          </div>
+
+          <div className="btn-container">
+            {activePj ? (
               <Link to="game">
-                <button type="submit" className="btn-play text-primary">
-                  Jugar
-                </button>
+                <button className="btn-play text-primary">Ir a Jugar</button>
               </Link>
+            ) : (
+              <button type="submit" className="btn-play text-primary">
+                Aceptar
+              </button>
+            )}
+
+            <div className="d-none">
+              <Game players={players} />
             </div>
           </div>
         </form>
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./Game.css";
 
-const Game = () => {
+const Game = (players) => {
+  console.log(players);
   const [turn, setTurn] = useState("X");
   const [cells, setCells] = useState(Array(9).fill(""));
   const [ganador, setGanador] = useState();
 
+  //componente de cell
   const Cell = ({ num }) => {
     return (
       <div onClick={() => handleClick(num)} className="cell">
@@ -15,38 +17,34 @@ const Game = () => {
   };
 
   const handleGame = (cuadrados) => {
-    let combinations = {
-      diag: [
-        [0, 4, 8],
-        [2, 4, 6],
-      ],
-      vert: [
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-      ],
-      horiz: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-      ],
-    };
-    for (let comb in combinations) {
-      combinations[comb].map((patron) => {
-        if (
-          cuadrados[patron[0]] === "" ||
-          cuadrados[patron[1]] === "" ||
-          cuadrados[patron[2]] === ""
-        ) {
-          //nada
-        } else if (
-          cuadrados[patron[0]] === cuadrados[patron[1]] &&
-          cuadrados[patron[1]] === cuadrados[patron[2]]
-        ) {
-          setGanador(cuadrados[patron[0]]);
-        }
-      });
-    }
+    //array de combinaciones para ganar
+    let combinations = [
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      ,
+    ];
+
+    //validacion que setea un ganador
+    combinations.map((comb) => {
+      if (
+        cuadrados[comb[0]] === "" ||
+        cuadrados[comb[1]] === "" ||
+        cuadrados[comb[2]] === ""
+      ) {
+        //nada
+      } else if (
+        cuadrados[comb[0]] === cuadrados[comb[1]] &&
+        cuadrados[comb[1]] === cuadrados[comb[2]]
+      ) {
+        setGanador(cuadrados[comb[0]]);
+      }
+    });
   };
 
   //funcion que maneja el estado del turno
@@ -64,9 +62,10 @@ const Game = () => {
     setCells(cuadrados);
   };
 
+  //funcion que retorna el array cells como inició
   const reset = () => {
     setGanador(null);
-    setCells(Array(9).fill(""));
+    return setCells(Array(9).fill(""));
   };
 
   return (
@@ -74,7 +73,11 @@ const Game = () => {
       <button onClick={reset} className="btn btn-primary">
         reset
       </button>
-      {ganador && <p className="text-center">{ganador} es el ganador</p>}
+      {/* {ganador && (
+        <p className="text-center">
+          {ganador === "X" ? name1 : name2} es el ganador
+        </p>
+      )} */}
       <div className="cell-container">
         <Cell num={0} />
         <Cell num={1} />
